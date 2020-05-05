56b5ed3e863cc0c684b16f35dde9dce0b5f5a090
Mike Jacobi <mikerjacobi@gmail.com>
2020-05-05 05:20:08 +0000

    prod twilio working!

--- server/go.mod
+++ server/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/aws/aws-sdk-go v1.30.19
 	github.com/davecgh/go-spew v1.1.1
 	github.com/google/uuid v1.1.1
+	github.com/gorilla/schema v1.1.0
 	github.com/husobee/vestigo v1.1.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
--- server/journal/handle_twilio_webhook/main.go
+++ server/journal/handle_twilio_webhook/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"context"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
+	"github.com/gorilla/schema"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/mikerjacobi/lambda-journal/server/common"
+	"github.com/mikerjacobi/lambda-journal/server/journal"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -20,44 +23,60 @@ type controller struct {
 }
 
 func (c controller) handleTwilioWebhook(ctx context.Context, req *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	lf := logrus.Fields{"handler": "handle_twilio_webhook"}
-
-	sess := session.Must(session.NewSession(&aws.Config{
-		Endpoint: aws.String("http://dev.jaqobi.com:8000"),
-		Region:   aws.String("us-west-2"),
-	}))
-
-	// Create DynamoDB client
-	svc := dynamodb.New(sess)
-	result, err := svc.PutItem(&dynamodb.PutItemInput{
-		TableName: aws.String("usersTable"),
-		Item: map[string]*dynamodb.AttributeValue{
-			"email": {S: aws.String("bob@lob.cob")},
-			"name": {
-				S: aws.String("bob"),
-			},
-		},
-	})
+	lf := logrus.Fields{"handler": c.Service}
+	entry, err := c.validate(ctx, req)
 	if err != nil {
+		logrus.WithError(err).Error("failed to validate entry")
+		return common.Response(ctx, http.StatusBadRequest, nil), nil
+	}
+
+	if _, err := c.DynamoDB.PutItem(entry.PutItem()); err != nil {
 		logrus.WithError(err).Error("failed to put item")
 		return common.Response(ctx, http.StatusOK, nil), nil
 	}
 
-	logrus.WithFields(lf).Infof("successfully handled twilio webhook %v", result)
-	return common.Response(ctx, http.StatusOK, nil), nil
+	logrus.WithFields(lf).Infof("successfully handled twilio webhook")
+	return common.Response(ctx, http.StatusOK, entry), nil
+}
+
+type TwilioInboundReq struct {
+	From string `schema:"From"`
+	Body string `schema:"Body"`
+}
+
+func (c controller) validate(ctx context.Context, req *events.APIGatewayProxyRequest) (journal.Entry, error) {
+	e := journal.Entry{
+		EntryID: "entry_" + uuid.New().String(),
+		Created: time.Now().Format(time.RFC3339),
+		Updated: time.Now().Format(time.RFC3339),
+	}
+	logrus.Infof("REQBODY %s", req.Body)
+	params, err := url.ParseQuery(req.Body)
+	if err != nil {
+		return e, errors.Wrap(err, "failed to parse query")
+	}
+	logrus.Infof("params %+v", params)
+	payload := TwilioInboundReq{}
+	decoder := schema.NewDecoder()
+	decoder.IgnoreUnknownKeys(true)
+	if err := decoder.Decode(&payload, params); err != nil {
+		return e, errors.Wrap(err, "failed to decode twilio payload")
+	}
+	logrus.Infof("payload %+v", payload)
+	e.Entry = payload.Body
+	return e, nil
 }
 
 func main() {
-	service := "handle-twilio-webhook"
-	c := controller{Controller: common.Controller{}}
+	c := controller{Controller: common.Controller{Service: "handle-twilio-webhook"}}
 	if err := envconfig.Process("journal", &c); err != nil {
 		logrus.WithError(err).Panicf("failed to load config")
 	}
+	common.Init(&c.Controller)
 
 	if c.Environment == "sandbox" {
 		c.Controller.ServeSandbox("/twilio", "POST", c.handleTwilioWebhook)
 	} else {
-		logrus.Infof("starting lambda %s server", service)
 		lambda.Start(c.handleTwilioWebhook)
 	}
 }
